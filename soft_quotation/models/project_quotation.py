2b83317a28722bbf8853b862d4237f5a35cb616a
hepan <916145267@qq.com>
2025-07-07 17:45:20 +0800

    报价模块

--- soft_quotation/models/project_quotation.py
+++ soft_quotation/models/project_quotation.py
@@ -19,25 +19,31 @@ class ProjectModule(models.Model):
     project_id = fields.Many2one('project.project', string='项目名')
     parent_id = fields.Many2one('project.module', string='上级模块',domain='[("id", "!=", id)]')
     name = fields.Char(string='模块名称')
-    level = fields.Integer(string='模块层级',compute='_compute_path_level')
-    path = fields.Char(string='模块路径',compute='_compute_path_level')
+    level = fields.Integer(string='模块层级',compute='_compute_path_level',store=True)
+    path = fields.Char(string='模块路径',compute='_compute_path_level',store=True)
+    complete_name = fields.Char(string='模块完整名称',compute='_compute_path_level',store=True)
     note = fields.Text(string='模块描述')
     status = fields.Selection(string='模块状态', selection = [
         ('draft', '草稿'),
         ('confirm', '确认'),
         ('done', '完成'),
     ],default='draft')
+    is_function_point = fields.Boolean('是否功能点',default=False)
+    is_story_point = fields.Boolean('是否故事点',default=False)
+
 
     @api.constrains('parent_id')
     def _check_parent_id(self):
         if self._has_cycle():
             raise ValidationError(_('不能设置循环的上级模块.'))
 
-    @api.depends('parent_id')
+    @api.depends('parent_id','note')
     def _compute_path_level(self):
         for record in self:
             if record.parent_id and record.parent_id.path:
                 record.path = record.parent_id.path + '/' + str(record.id)
+                record.complete_name = record.parent_id.complete_name + '/' + record.name
             else:
                 record.path = '/' + str(record.id)
+                record.complete_name = record.name
             record.level = record.path.count('/')
