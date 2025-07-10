9586f244d27f02f13b1143625dac2a1b8e6bcab4
hepan <916145267@qq.com>
2025-07-10 14:14:26 +0800

    人才区域添加年份

--- soft_quotation/models/region_hr.py
+++ soft_quotation/models/region_hr.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-from email.policy import default
-
+from datetime import datetime
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 
@@ -12,10 +11,18 @@ class RegionHr(models.Model):
     def _default_country(self):
         record = self.env['res.country'].sudo().search([('code', '=', 'CN')],limit=1)
         return record.id if record else False
+    def _get_years(self):
+        """ 动态生成最近10年的年份选项 """
+        current_year = datetime.now().year
+        return [(str(year), str(year)) for year in range(current_year - 10, current_year + 1)]
 
     job_position = fields.Many2one('system.data.dictionary',string='职位名称',domain='[("category", "=", "职位名称")]')
     res_country = fields.Many2one('res.country', string='国家',default=_default_country)
     province = fields.Many2one('res.country.state', string='省',domain="[('country_id', '=', res_country)]")
     city = fields.Many2one('res.city', string='城市',domain="[('state_id', '=', province)]")
+    year = fields.Selection(
+        selection=lambda self: self._get_years(),
+        string='年份'
+    )
     job_level = fields.Many2one('system.data.dictionary',string='职位级别',domain='[("category", "=", "职位级别")]')
     average_salary = fields.Float(string='平均薪资(K)', help = '该平均薪资单位为K')
