ceab46b44c29b5221046c4cd9fc0d6dc620dacf5
hepan <916145267@qq.com>
2025-07-10 11:02:53 +0800

    生产效率添加

--- soft_quotation/models/software_scale.py
+++ soft_quotation/models/software_scale.py
@@ -23,18 +23,21 @@ class SoftwareScale(models.Model):
             ('name', '=', '估算')
         ], limit=1)
         return record.id if record else False
-
+    def _get_profession(self):
+        record = self.env['system.data.dictionary'].sudo().search([('category', '=', '行业'),('name', '=', '全行业')], limit=1)
+        return record.id if record else False
     project_id = fields.Many2one('project.project', string='项目名')
+    profession = fields.Many2one('system.data.dictionary',string='行业',domain='[("category","=","行业")]',default=_get_profession)
     # stage_id = fields.Many2one('project_id.stage_id', string='阶段')
     name = fields.Char(string='软件规模名称',related='project_id.name')
     # 评估方式 之间的转换 1故事点 ≈ 5 功能点 ≈ 250行代码
     scale_type =fields.Selection([('function_point', '功能点'),
                                   ('story_point', '故事点'),
                                   ('loc', 'LOC')], '评估方式',
-                                 default='function_point')
+                                 tracking=True,default='function_point')
     # 复杂度
-    complexity = fields.Many2one('system.data.dictionary',string='复杂度',domain='[("category", "=", "软件规模复杂度")]',default=_default_complexity)
-    quotation_type = fields.Many2one('system.data.dictionary',string='报价精度',domain='[("category", "=", "软件报价精度")]',default=_default_quotation_type)
+    complexity = fields.Many2one('system.data.dictionary',string='复杂度',tracking=True,domain='[("category", "=", "软件规模复杂度")]',default=_default_complexity)
+    quotation_type = fields.Many2one('system.data.dictionary',string='报价精度',tracking=True,domain='[("category", "=", "软件报价精度")]',default=_default_quotation_type)
 
     # 项目的报价精度
     # quotation_type = fields.Selection([
@@ -46,8 +49,10 @@ class SoftwareScale(models.Model):
         ('draft', '草稿'),
         ('confirm', '确认'),
         ('done', '完成'),
-    ],'规模状态',default='draft')
-    scale_amount = fields.Integer(string='软件规模数')
+    ],'规模状态',default='draft',tracking=True)
+    cf = fields.Float(string='变更调整因子',tracking=True, help='例如某项目UFP为1000FP，预计50%变更，则CF=1.5，S=1500FP', store=True,default=1)
+    ufp = fields.Float(string='未调整功能点(UFP)', compute='_compute_fp', store=True)
+    scale_amount = fields.Float(string='调整后功能点(FP)',compute='_compute_fp',store=True)
     scl_ids = fields.One2many('software.scale.line', 'software_scale_id', string='软件规模明细')
     show_function = fields.Boolean(string='是否显示功能点数',default=True,compute='_compute_show_type',store=True)
     show_story = fields.Boolean(string='是否显示故事点数',default=False,compute='_compute_show_type',store=True)
@@ -68,6 +73,11 @@ class SoftwareScale(models.Model):
                 record.show_function = False
                 record.show_story = False
                 record.show_loc = True
+    @api.depends('scl_ids','scl_ids.amount', 'cf')
+    def _compute_fp(self):
+        for record in self:  # 只需操作主记录
+            record.ufp = sum([item.amount for item in record.scl_ids])
+            record.scale_amount = record.ufp * record.cf
 
     def import_project_module(self):
         logging.info('导入项目模块')
@@ -89,6 +99,7 @@ class SoftwareScale(models.Model):
             )
         self.scl_ids = scl_lines
         pass
+
 
 class SoftwareScaleLine(models.Model):
     _name = 'software.scale.line'
--- soft_quotation/models/software_work_volume.py
+++ soft_quotation/models/software_work_volume.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+from datetime import datetime
 from odoo import models, fields, api
 
 
@@ -8,4 +8,23 @@ class WorkVolume(models.Model):
     _description = '工作量'
 
 
+class ProductionDateRate(models.Model):
+    _name = 'software.production.date.rate'
+    _description = '生产率'
 
+    def _get_years(self):
+        """ 动态生成最近10年的年份选项 """
+        current_year = datetime.now().year
+        return [(str(year), str(year)) for year in range(current_year - 10, current_year + 1)]
+    profession = fields.Many2one('system.data.dictionary',string='行业',domain='[("category","=","行业")]')
+    soft_stage = fields.Many2one('system.data.dictionary',string='软件阶段',domain='[("category","=","软件阶段")]')
+    # city = fields.Many2one('res.city', string='城市')
+    year = fields.Selection(
+        selection=lambda self: self._get_years(),
+        string='年份'
+    )
+    p10 = fields.Float(string='P10',help='单位:人时/功能点')
+    p25 = fields.Float(string='P25', help='单位:人时/功能点')
+    p50 = fields.Float(string='P50', help='单位:人时/功能点')
+    p75 = fields.Float(string='P75', help='单位:人时/功能点')
+    p90 = fields.Float(string='P90', help='单位:人时/功能点')
